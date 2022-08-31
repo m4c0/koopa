1b4f14348b4b9b1be2a5b134e9b93fbcb3fb8ab2
Eduardo Costa <m4c0@users.noreply.github.com>
2022-08-31 17:25:44 +0100

    copying remaining bits from oto

---
+++ inc/koopa/constants.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "koopa/io.hpp"
+
+namespace koopa {
+  template<typename T>
+  static constexpr auto constant(T && t) noexcept {
+    return [t = std::forward<T>(t)](input in) noexcept {
+      return output<T> { t, in };
+    };
+  }
+
+  template<typename T>
+  static constexpr auto failure(std::string_view sv) noexcept {
+    return [sv](input in) noexcept {
+      return fail<T>(sv, in);
+    };
+  }
+
+  static constexpr auto eof(input in) noexcept {
+    return in ? fail<nullptr_t>("expecting eof", in) : output<nullptr_t> { nullptr, in };
+  }
+}
---
+++ inc/koopa/matcher.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "koopa/io.hpp"
+
+namespace koopa {
+  static constexpr auto match(char chr) noexcept {
+    return [chr](const input in) noexcept {
+      if (!in) return fail<char>("eof while waiting for char", in);
+      if (in.peek() != chr) return fail<char>("mismatched char", in);
+      return output<char> { chr, in.take(1) };
+    };
+  }
+
+  static constexpr auto match(std::string_view sv) noexcept {
+    return [sv](const input in) noexcept {
+      const auto len = sv.length();
+      if (in.peek(len) != sv) return fail<std::string_view>("mismatched string", in);
+      return output<std::string_view> { sv, in.take(sv.length()) };
+    };
+  }
+}
