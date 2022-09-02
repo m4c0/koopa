37de984095a9be0a21e216bc5b41f66de4476943
Eduardo Costa <m4c0@users.noreply.github.com>
2022-09-02 13:37:39 +0100

    io: new test cases

--- src/io.cpp
+++ src/io.cpp
@@ -30,4 +30,6 @@ static_assert(fail<char>("ok", "cc"_i) != fail<char>("ok", "bb"_i));
 static_assert(fail<char>("nok", "bb"_i) != fail<char>("ok", "bb"_i));
 
 static_assert(output { 'a', "b"_i }.with_value('c') == output { 'c', "b"_i });
+static_assert(fail<char>("ok", "bb"_i).with_error("nok") == fail<char>("nok", "bb"_i));
 static_assert(fail<char>("ok", "bb"_i).with_error_type<int>() == fail<int>("ok", "bb"_i));
+static_assert(fail<char>("ok", "bb"_i).with_cause<int>("ish") == fail<int>("ok\ncaused by: ish", "bb"_i));
