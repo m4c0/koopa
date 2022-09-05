c203e93eedd1afe5bf461738a40ee83e35922068
Eduardo Costa <m4c0@users.noreply.github.com>
2022-09-05 17:22:18 +0100

    binary: moving code around

--- src/binary.cpp
+++ src/binary.cpp
@@ -1,61 +1,12 @@
-#include "koopa/io.hpp"
+#include "koopa/binary.hpp"
+#include "koopa/combiner.hpp"
 
 #include <bit>
 
+using namespace koopa;
+
 // Some tests are easier if defined as LE
 static_assert(std::endian::native == std::endian::little);
-
-namespace koopa {
-  [[nodiscard]] inline constexpr auto take_u8() noexcept {
-    return [](input in) noexcept -> output<uint8_t> {
-      if (!in) return fail<uint8_t>("eof while expecting u8", in);
-      return { static_cast<uint8_t>(in.peek()), in.take(1) };
-    };
-  }
-  [[nodiscard]] inline constexpr auto take_u16le() noexcept {
-    return [](input in) noexcept -> output<uint16_t> {
-      const auto sv = in.peek(2);
-      if (sv.size() != 2) return fail<uint16_t>("eof while expecting u16le", in);
-      const auto a = static_cast<uint16_t>(static_cast<uint8_t>(sv[0]));
-      const auto b = static_cast<uint16_t>(static_cast<uint8_t>(sv[1]) << 8U);
-      return { static_cast<uint16_t>(b | a), in.take(2) };
-    };
-  }
-  [[nodiscard]] inline constexpr auto take_u32le() noexcept {
-    return [](input in) noexcept -> output<uint32_t> {
-      const auto sv = in.peek(4);
-      if (sv.size() != 4) return fail<uint32_t>("eof while expecting u32le", in);
-      const auto a = static_cast<uint32_t>(static_cast<uint8_t>(sv[0]));
-      const auto b = static_cast<uint32_t>(static_cast<uint8_t>(sv[1]) << 8U);
-      const auto c = static_cast<uint32_t>(static_cast<uint8_t>(sv[2]) << 16U);
-      const auto d = static_cast<uint32_t>(static_cast<uint8_t>(sv[3]) << 24U);
-      return { d | c | b | a, in.take(4) };
-    };
-  }
-
-  [[nodiscard]] inline constexpr auto match_u8(uint8_t exp) noexcept {
-    return [exp, m = take_u8()](input in) noexcept {
-      auto got = m(in);
-      return (!got || *got == exp) ? got : fail<uint8_t>("mismatched u8", in);
-    };
-  }
-
-  [[nodiscard]] inline constexpr auto match_u16le(uint16_t exp) noexcept {
-    return [exp, m = take_u16le()](input in) noexcept {
-      auto got = m(in);
-      return (!got || *got == exp) ? got : fail<uint16_t>("mismatched u16le", in);
-    };
-  }
-
-  [[nodiscard]] inline constexpr auto match_u32le(uint32_t exp) noexcept {
-    return [exp, m = take_u32le()](input in) noexcept {
-      auto got = m(in);
-      return (!got || *got == exp) ? got : fail<uint32_t>("mismatched u32le", in);
-    };
-  }
-}
-
-using namespace koopa;
 
 static constexpr const auto t_u8 = take_u8();
 static_assert(t_u8(""_i) == fail<uint8_t>("eof while expecting u8", ""_i));
@@ -87,8 +38,6 @@ static_assert(t_mu16le("\1\1eh"_i) == output<uint16_t> { 257, "eh"_i });        
 static constexpr const auto t_mu32le = match_u32le(0x01020304);                              // NOLINT
 static_assert(t_mu32le("\1\1\1\1h"_i) == fail<uint32_t>("mismatched u32le", "\1\1\1\1h"_i)); // NOLINT
 static_assert(t_mu32le("\4\3\2\1h"_i) == output<uint32_t> { 0x01020304, "h"_i });            // NOLINT
-
-#include "koopa/combiner.hpp"
 
 struct wad_header {
   uint32_t num_lumps;
